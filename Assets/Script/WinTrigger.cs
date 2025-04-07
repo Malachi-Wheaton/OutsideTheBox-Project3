54c381c08f3ef8fe2e23ba1ffb7f16752cc8e9ac
Malachi Wheaton <mwheaton@ltu.edu>
2025-04-07 09:32:17 -0400

    First Working Level

---
+++ Assets/Script/WinTrigger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+
+public class GoalTrigger2D : MonoBehaviour
+{
+    public TMP_Text winText;
+
+    void Start()
+    {
+        winText.text = "";
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Weight"))
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
