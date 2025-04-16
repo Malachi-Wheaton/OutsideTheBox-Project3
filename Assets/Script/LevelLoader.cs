ef9feaf6c5fc70a0077b3b58ba959717bb4d2377
Malachi Wheaton <mwheaton@ltu.edu>
2025-04-16 12:59:15 -0400

    Still some touching up to do

--- Assets/Script/LevelLoader.cs
+++ Assets/Script/LevelLoader.cs
@@ -5,19 +5,22 @@ public class LevelLoader : MonoBehaviour
 {
     public void LoadNextLevel()
     {
+        Time.timeScale = 1f; 
+
         int currentIndex = SceneManager.GetActiveScene().buildIndex;
         int nextIndex = currentIndex + 1;
 
-        
         if (nextIndex < SceneManager.sceneCountInBuildSettings)
         {
             SceneManager.LoadScene(nextIndex);
         }
         else
         {
-            Debug.Log("No more levels!"); 
+            Debug.Log("No more levels!");
+            SceneManager.LoadScene("MainMenuScene");
         }
     }
 }
+
 
 
